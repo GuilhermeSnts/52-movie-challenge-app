ae792903f739386370ed007cf72224a313809211
Guilherme Santos <guilhermess1994@gmail.com>
2020-03-22 22:27:17 -0300

    use pug,sass and atomic design

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  div (class=".home")
+    Home
+</template>
+
+<script>
+// @ is an alias to /src
+import Home from "../templates/Home.vue";
+export default {
+  name: "Home",
+  components: {
+    Home
+  }
+};
+</script>
---
+++ src/components/templates/Home.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  div (class="home")
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
