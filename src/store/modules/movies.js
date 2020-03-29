a92b762f355079393d69a55161e9681a36658ad0
guilhermesnts <guilhermess1994@gmail.com>
2020-03-29 20:04:56 -0300

    Added vuex actions, states, mutations and getters to search movies

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,21 +1,41 @@
+import axios from "axios";
+
 export default {
   namespaced: true,
   state: {
-    movieList: []
+    guestSession: "",
+    movieList: [],
+    movieApiKey: ""
   },
   getters: {
     getMoviesList(state) {
       return state.movieList;
+    },
+    getMovieApiKey(state) {
+      return state.movieApiKey;
     }
   },
   mutations: {
-      setMovieList(store, payload){
-          store.movieList = payload
-      }
+    setMovieList(store, payload) {
+      store.movieList = payload;
+    },
+    setGuestSession(store, payload) {
+      store.guestSession = payload;
+    }
   },
   actions: {
-    searchMovies({ commit }, payload) {
-        commit("setMovieList", payload)
+    async searchMovies({ commit, getters }, payload) {
+      const termsToSearch = payload.replace(/ /g, "+");
+      const uri = `https://api.themoviedb.org/3/search/movie?api_key=${getters.getMovieApiKey}&query=${termsToSearch}`;
+      const search = await axios(uri).then(res => res.data.results);
+      const movieList = search.map(m => ({ title: m.title, id: m.id }));
+      commit("setMovieList", movieList);
+    },
+
+    async createTMDBSession({ commit, getters }) {
+      const uri = `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${getters.getMovieApiKey}`;
+      const session = await fetch(uri).then(res => res.data);
+      commit("setGuestSession", session);
     }
   }
 };
